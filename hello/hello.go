f88d085f165488f0f4fee51be138834ab0e13ca9
Kanui <marley.kanui@gmail.com>
2020-11-13 19:20:13 -0600

    go-docs-create-module - global commit 05 - Updated modules to add an arraywith dynamic arguments and assign them to a Go map

--- hello/hello.go
+++ hello/hello.go
@@ -14,8 +14,11 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
+    // A slice of names.
+    names := []string{"Marley", "Kanui", "isDope"}
+
     // Request a greeting message.
-    message, err := greetings.Hello("Marley")
+    message, err := greetings.Hellos(names)
     // If an error was returned, print it to the console and
     // exit the program.
     if err != nil {
